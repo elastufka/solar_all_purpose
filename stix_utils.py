9cfc81000fecf438e2bc6cb6f1feaed3257a21c6
elastufka <elastufka@gmail.com>
2022-05-18 12:10:16 +0200

    fix type

--- stix_utils.py
+++ stix_utils.py
@@ -526,5 +526,5 @@ def process_image_fits(infile):
     hdict['hpc_x_arcsec']=hpj_cs[0].Tx.value
     hdict['hpc_y_arcsec']=hpj_cs[0].Ty.value
     hdict['maxpix']=argmax2D(mm.data) #cross-check
-    hdidct['s2n']=np.nanmax(mm.data)/np.nanstd(mm.data)
+    hdict['s2n']=np.nanmax(mm.data)/np.nanstd(mm.data)
     return hdict
