eee906d792fc8ab605a8aca9c91c88fdfdbf315d
elastufka <elastufka@gmail.com>
2022-08-22 15:05:29 +0200

    updates to work with jupyterlab

--- xspec_utils.py
+++ xspec_utils.py
@@ -73,7 +73,7 @@ def spectrum_from_time_interval(original_fitsfile, start_time, end_time, out_fit
 
 #def select_background_interval():
 
-def fit_thermal_nonthermal(xspec, ntmodel = 'bknpower', lowErange = [2.0,10.0], highErange = [8.0,30.0], breakEstart = 15, minCounts=10, statMethod='chi',query='no',renorm=True,nIterations = 1000):
+def fit_thermal_nonthermal(xspec, ntmodel = 'bknpower', lowErange = [2.0,10.0], highErange = [8.0,30.0], breakEstart = 15, breakEfrozen=False, minCounts=10, statMethod='chi',query='no',renorm=True,nIterations = 1000):
     '''Fit thermal and non-thermal components to spectrum via the following steps:
         1) fit thermal over low energy
         2) fit non-thermal over high-energy with initial break energy frozen (if non-thermal model is bknpow or thick2)
@@ -81,7 +81,8 @@ def fit_thermal_nonthermal(xspec, ntmodel = 'bknpower', lowErange = [2.0,10.0], 
         3) fit thermal and non-thermal together over entire energy range'''
     breakE = True #assume there's a break energy
     xspec.Xset.abund="felc"
-    xspec.AllModels.clear()
+    if ntmodel != 'thick2':
+        xspec.AllModels.clear() #for now...
     #settings for fit
     xspec.Fit.statMethod = statMethod #Valid names: 'chi' | 'cstat' | 'lstat' | 'pgstat' | 'pstat' | 'whittle'.
     xspec.Fit.query = query
@@ -103,12 +104,22 @@ def fit_thermal_nonthermal(xspec, ntmodel = 'bknpower', lowErange = [2.0,10.0], 
     set_xspec_model_params(m, 'apec', mtherm_params, frozen = True)
         
     m_nt = getattr(m,ntmodel)
-    try:
+    try: #in thick2 it's eebrk not BreakE...
         breakEindex = getattr(m_nt, 'BreakE')._Parameter__index
         m.setPars({breakEindex:f"{breakEstart} -.5,,,{breakEstart+2}"})
-        p = getattr(m_nt,'BreakE')
+        breakparname = 'BreakE'
+        #p = getattr(m_nt,'BreakE')
     except AttributeError:
-        breakE = False
+        try:
+            breakEindex = getattr(m_nt, 'eebrk')._Parameter__index
+            m.setPars({breakEindex:f"{breakEstart} -.5,,,{breakEstart+2}"})
+            breakparname = 'eebrk'
+            lowEindex = getattr(m_nt, 'eelow')._Parameter__index
+            m.setPars({lowEindex:f"{breakEstart-5} -.5,,,{breakEstart-2}"})
+            p = getattr(getattr(m,ntmodel),'eelow')
+            p.frozen = False
+        except AttributeError:
+            breakE = False
  
     xspec.AllData.notice('all')
     #check that count rate at highErange is above minCounts, otherwise adjust highErange and warn
@@ -120,8 +131,8 @@ def fit_thermal_nonthermal(xspec, ntmodel = 'bknpower', lowErange = [2.0,10.0], 
     xspec.Fit.renorm()
     xspec.Fit.perform()
 
-    if breakE: #fit again with unfrozen break E
-        p = getattr(getattr(m,ntmodel),'BreakE')
+    if breakE and not breakEfrozen: #fit again with unfrozen break E
+        p = getattr(getattr(m,ntmodel),breakparname)
         p.frozen = False
         xspec.Fit.renorm()
         xspec.Fit.perform()
@@ -165,10 +176,11 @@ def get_xspec_model_sigmas(model_component):
     Input: xspec Component object'''
     return tuple([getattr(model_component,p).sigma for p in model_component.parameterNames])
         
-def show_model(model):
+def show_model(model, df=False):
     '''equivalant of pyxspec show() but in Markdown for Jupyter
     Input: xspec Model object'''
     mdtable="|Model par| Model comp | Component|  Parameter|  Unit |    Value| Sigma |\n |---|---|---|---|---|---| |\n"
+    pdict={'Model par':[], 'Model comp':[], 'Component':[], 'Parameter': [], 'Unit': [], 'Value': [], 'Sigma':[]}
     for i,n in enumerate(model.componentNames):
         nprev=0
         if i>0:
@@ -187,7 +199,18 @@ def show_model(model):
             else:
                 plusminus= f"± {getattr(param,'sigma'):{fmt}}"
             mdtable+=f"|{j+1+nprev} |{i+1} | {n}|{p}| {getattr(param,'unit')}| {getattr(param,'values')[0]:{fmt}} | {plusminus}|\n"
-    return Markdown(mdtable)
+            pdict['Model par'].append(j+1+nprev)
+            pdict['Model comp'].append(i+1)
+            pdict['Component'].append(n)
+            pdict['Parameter'].append(p)
+            pdict['Unit'].append(getattr(param,'unit'))
+            pdict['Value'].append(f"{getattr(param,'values')[0]:{fmt}}")
+            pdict['Sigma'].append(plusminus)
+            
+    if df: #jupyterlab doesn't work with Mardown for whatever reason, but it will display a dataframe nicely
+        return pd.DataFrame(pdict)
+    else:
+        return Markdown(mdtable)
     
 def show_error(model):
     '''show parameters and errors. Input: xspec Model object'''
@@ -250,7 +273,7 @@ def plot_data(xspec,fitrange=False, dataGroup=1,erange=False, counts=False, titl
     fig.update_layout(title = title)
     return fig
 
-def plot_fit(xspec, model, fitrange=False, dataGroup=1,erange=False,yrange = [-3, 4], res_range=[-50,50],title=False,annotation=False, plotdata_dict = False):
+def plot_fit(xspec, model, fitrange=False, dataGroup=1,erange=False,yrange = [-3, 4], res_range=[-50,50],title=False,annotation=False, plotdata_dict = False, width = 500, height = 700):
     '''plot data, fit, residuals in PlotLy. Input: xspec global object
     Plot from dictionary of plot parameters if xspec = None, model = plotadata '''
     
@@ -306,14 +329,14 @@ def plot_fit(xspec, model, fitrange=False, dataGroup=1,erange=False,yrange = [-3
 
     #plot residuals
     fig.update_yaxes(type='log',row=1,col=1,showexponent = 'all',exponentformat = 'e',range=yrange)
-    fig.add_trace(go.Scatter(x=xx,y=res,mode='markers',marker_color='brown',name='residuals'),row=2,col=1)
+    fig.add_trace(go.Scatter(x=xx,y=res,mode = 'lines+markers',marker_color='brown',name='residuals',line_shape = 'hv'),row=2,col=1)
     fig.add_vrect(x0=fitrange[0],x1=fitrange[1],annotation_text='fit range',fillcolor='lightgreen',opacity=.25,line_width=0,row=1,col=1)
     fig.add_vrect(x0=fitrange[0],x1=fitrange[1],fillcolor='lightgreen',opacity=.25,line_width=0,row=2,col=1)
     if annotation:
         fig.add_annotation(x=1.25,y=.5,text=annotation,align='left',xref='x domain',yref='paper')
     fig.update_yaxes(title='Residuals',range=res_range,row=2,col=1)
     fig.update_xaxes(title='Energy (keV)',range=erange,row=2,col=1)
-    fig.update_layout(width=500,height=700,title=title)
+    fig.update_layout(width=width,height=height,title=title)
     
     if plotdata_dict: #return plot data in a dictionary
         fitdata = {'fitrange':fitrange}
